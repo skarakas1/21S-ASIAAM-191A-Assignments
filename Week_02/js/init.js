866f4ff099764b5c024fb9fdef66f94bacb35cf1
skarakas1 <skarakas21@ucla.edu>
2021-04-14 17:49:36 -0700

    Updates to Week 2 Lab

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -6,17 +6,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-//JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map)
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
-		// .openPopup();
-
-        fetch("lab1.geojson")
-        .then(response => {
-            return response.json();
-        })
-        .then(data =>{
-            // Basic Leaflet method to add GeoJSON data
-            L.geoJSON(data).addTo(map)
+// adding GeoJSON
+fetch("js/map.geojson")
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+        // Basic Leaflet method to add GeoJSON data
+                        // the leaflet method for adding a geojson
+            L.geoJSON(data, {
+                style: function (feature) {
+                    return {color: 'red'};
+                }
+            }).bindPopup(function (layer) {
+                return layer.feature.properties.place;
+            }).addTo(map);
         });
-
